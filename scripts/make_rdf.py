1be346dc4c34b550c91b027b97b3cef1ea092b8e
csae8092 <peter.andorfer@oeaw.ac.at>
2023-03-23 17:56:16 +0100

    serialize placeNames as E33 with appelations and labels

--- scripts/make_rdf.py
+++ scripts/make_rdf.py
@@ -5,13 +5,13 @@ from acdh_cidoc_pyutils import (
     make_e42_identifiers,
     make_birth_death_entities,
     make_occupations,
-    make_affiliations,
     make_entity_label,
+    normalize_string
 )
 from acdh_cidoc_pyutils.namespaces import CIDOC
 from acdh_tei_pyutils.tei import TeiReader
-from rdflib import Graph, Namespace, URIRef
-from rdflib.namespace import RDF
+from rdflib import Graph, Namespace, URIRef, Literal
+from rdflib.namespace import RDF, RDFS
 
 if os.environ.get("NO_LIMIT"):
     LIMIT = False
@@ -35,9 +35,24 @@ for x in doc.any_xpath('.//tei:person//tei:placeName'):
 place_names = list(place_names)
 for x in doc.any_xpath('.//tei:person//tei:placeName'):
     n = place_names.index(x.text)
-    place_name_id = f"#fa_place{n:08}"
-    x.attrib["key"] = place_name_id
-
+    place_name_id = f"fa_place{n:08}"
+    x.attrib["key"] = f"#{place_name_id}"
+    subj = URIRef(f"{SK}{place_name_id}")
+    g.add((
+        subj, RDF.type, CIDOC["E53_Place"]
+    ))
+    g.add((
+        subj, RDFS.label, Literal(f"Place: {x.text}", lang="en")
+    ))
+    app_uri = URIRef(f"{subj}/appellation/0")
+    g.add((subj, CIDOC["P1_is_identified_by"], app_uri))
+    g.add((app_uri, RDF.type, CIDOC["E33_E41_Linguistic_Appellation"]))
+    g.add(
+        (app_uri, RDFS.label, Literal(normalize_string(x.text), lang="de"))
+    )
+    g.add(
+        (app_uri, RDF.value, Literal(normalize_string(x.text)))
+    )
 items = doc.any_xpath(f".//tei:{entity_type}")
 if LIMIT:
     items = items[:LIMIT]
@@ -52,14 +67,6 @@ for x in tqdm(items, total=len(items)):
     g += make_e42_identifiers(subj, x, type_domain=f"{SK}types", default_lang="und", same_as=False)
     g += make_appellations(subj, x, type_domain=f"{SK}types", default_lang="und")
     g += make_occupations(subj, x, default_lang="de")[0]
-    g += make_affiliations(
-        subj,
-        x,
-        domain,
-        person_label=item_label,
-        org_id_xpath="./tei:orgName[1]/@key",
-        org_label_xpath="./tei:orgName[1]//text()",
-    )
     birth_g, birth_uri, birth_timestamp = make_birth_death_entities(
         subj,
         x,
