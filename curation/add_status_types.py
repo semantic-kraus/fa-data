92c6f4c4a89296d7c913e61291d7245d6f5340bd
csae8092 <peter.andorfer@oeaw.ac.at>
2023-08-23 21:13:07 +0200

    now set status checked to all gnd items

--- curation/add_status_types.py
+++ curation/add_status_types.py
@@ -5,15 +5,19 @@ index_file = "./data/indices/listperson.xml"
 break_id = "p_46270" # matches line nr. 95876, see https://github.com/semantic-kraus/fa-data/commit/64f154275403317bb040e3166d1dd3f8389dc1b6
 
 doc = TeiReader(index_file)
+nsmap = doc.nsmap
 
-for x in doc.any_xpath(".//tei:listPerson/tei:person[@xml:id]"):
-    entity_id = x.attrib["{http://www.w3.org/XML/1998/namespace}id"]
-    x.attrib["status"] = "todo"
+# for x in doc.any_xpath(".//tei:listPerson/tei:person[@xml:id]"):
+#     entity_id = x.attrib["{http://www.w3.org/XML/1998/namespace}id"]
+#     x.attrib["status"] = "todo"
 
-for x in doc.any_xpath(".//tei:listPerson/tei:person[@xml:id]"):
-    entity_id = x.attrib["{http://www.w3.org/XML/1998/namespace}id"]
-    x.attrib["status"] = "checked"
-    if entity_id == break_id:
-        break
-    
-doc.tree_to_file(index_file)
+# for x in doc.any_xpath(".//tei:listPerson/tei:person[@xml:id]"):
+#     entity_id = x.attrib["{http://www.w3.org/XML/1998/namespace}id"]
+#     x.attrib["status"] = "checked"
+#     if entity_id == break_id:
+#         break
+
+for x in doc.any_xpath(".//tei:listPerson/tei:person[@status='todo']"):
+    if x.xpath('./tei:idno[@type="GND" or @type="WikiData"]', namespaces=nsmap):
+        entity_id = x.attrib["{http://www.w3.org/XML/1998/namespace}id"]
+        x.attrib["status"] = "checked"
