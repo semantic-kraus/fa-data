eb8a36c3107c5591f2094a33f5c95b7b20863534
csae8092 <peter.andorfer@oeaw.ac.at>
2023-08-23 22:06:12 +0200

    status

--- curation/add_status_types.py
+++ curation/add_status_types.py
@@ -2,21 +2,21 @@ from acdh_tei_pyutils.tei import TeiReader
 
 
 index_file = "./data/indices/listperson.xml"
-break_id = "p_46270"  # matches line nr. 95876
-# see https://github.com/semantic-kraus/fa-data/commit/64f154275403317bb040e3166d1dd3f8389dc1b6
+break_id = "p_46555" 
+# see https://github.com/semantic-kraus/fa-data/commit/e279e4ad182c14e15a03825ed3e32e1d327614e5
 
 doc = TeiReader(index_file)
 nsmap = doc.nsmap
 
-# for x in doc.any_xpath(".//tei:listPerson/tei:person[@xml:id]"):
-#     entity_id = x.attrib["{http://www.w3.org/XML/1998/namespace}id"]
-#     x.attrib["status"] = "todo"
+for x in doc.any_xpath(".//tei:listPerson/tei:person[@xml:id]"):
+    entity_id = x.attrib["{http://www.w3.org/XML/1998/namespace}id"]
+    x.attrib["status"] = "todo"
 
-# for x in doc.any_xpath(".//tei:listPerson/tei:person[@xml:id]"):
-#     entity_id = x.attrib["{http://www.w3.org/XML/1998/namespace}id"]
-#     x.attrib["status"] = "checked"
-#     if entity_id == break_id:
-#         break
+for x in doc.any_xpath(".//tei:listPerson/tei:person[@xml:id]"):
+    entity_id = x.attrib["{http://www.w3.org/XML/1998/namespace}id"]
+    x.attrib["status"] = "checked"
+    if entity_id == break_id:
+        break
 
 for x in doc.any_xpath(".//tei:listPerson/tei:person[@status='todo']"):
     if x.xpath('./tei:idno[@type="GND" or @type="WikiData"]', namespaces=nsmap):
