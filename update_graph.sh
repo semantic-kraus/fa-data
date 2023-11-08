af7973090d54b9e2f922dbb592a357719dd5d817
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2023-11-08 13:57:28 +0100

    increased sleep time

--- update_graph.sh
+++ update_graph.sh
@@ -4,7 +4,7 @@ echo "delete namedgraphs"
 curl -D- -X DELETE \
     -u $R_USER \
     'https://sk-blazegraph.acdh-dev.oeaw.ac.at/blazegraph/sparql?c=<https://sk.acdh.oeaw.ac.at/project/fackel>&c=<https://sk.acdh.oeaw.ac.at/provenance>&c=<https://sk.acdh.oeaw.ac.at/model>&c=<https://sk.acdh.oeaw.ac.at/general>'
-sleep 60
+sleep 300
 
 echo "upload namedgraphs data.trig"
 curl -u $R_USER \
@@ -12,7 +12,7 @@ curl -u $R_USER \
     -H 'Content-Type: application/x-trig; charset=UTF-8' \
     -H 'Accept: text/boolean' \
     -d @rdf/data.trig
-sleep 60
+sleep 600
 
 echo "namedgraphs texts.trig"
 curl -u $R_USER \
@@ -20,7 +20,7 @@ curl -u $R_USER \
     -H 'Content-Type: application/x-trig; charset=UTF-8' \
     -H 'Accept: text/boolean' \
     -d @rdf/texts.trig
-sleep 60
+sleep 600
 
 echo "namedgraphs mentions1.trig"
 curl -u $R_USER \
@@ -28,7 +28,7 @@ curl -u $R_USER \
     -H 'Content-Type: application/x-trig; charset=UTF-8' \
     -H 'Accept: text/boolean' \
     -d @rdf/mentions1.trig
-sleep 60
+sleep 600
 
 echo "namedgraphs mentions2.trig"
 curl -u $R_USER \
@@ -36,7 +36,7 @@ curl -u $R_USER \
     -H 'Content-Type: application/x-trig; charset=UTF-8' \
     -H 'Accept: text/boolean' \
     -d @rdf/mentions2.trig
-sleep 60
+sleep 600
 
 echo "namedgraphs mentions3.trig"
 curl -u $R_USER \
