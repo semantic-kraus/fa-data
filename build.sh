604c7d741c5ef65e37015d4c17b68addf1e4372f
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2023-11-09 12:46:53 +0100

    added wf to create gh pages

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
+# bin/bash
+
 rm -rf html
 mkdir html
 touch html/.nojekyll
 
 python scripts/make_rdf.py
-
-# python scripts/make_index.py
-# cp rdf/*.ttl html/
-# cp rdf/*.trig html/
---
+++ create_pages.sh
@@ -0,0 +1,5 @@
+# bin/bash
+
+python scripts/make_index.py
+cp rdf/*.ttl html/
+cp rdf/*.trig html/
