1a1bc956daac9d34dc3f850a3d7f87a05b2271ff
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2023-11-29 15:22:01 +0100

    reverting upload script

--- update_graph.sh
+++ update_graph.sh
@@ -1,26 +1,30 @@
 # bin/bash
 
 echo "delete namedgraphs"
-curl -D- -X DELETE "${R_ENDPOINT_V}?c=<https://sk.acdh.oeaw.ac.at/project/fackel>"
+curl -D- -X DELETE \
+    "${R_ENDPOINT_V}?c=<https://sk.acdh.oeaw.ac.at/project/fackel>"
 sleep 300
 
 echo "upload namedgraphs data.trig"
-curl -D- -H 'Content-Type: application/x-turtle; charset=UTF-8' \
-    -d @rdf/data.trig \
-    -X POST $R_ENDPOINT_V
+curl $R_ENDPOINT_V \
+    -H 'Content-Type: application/x-trig; charset=UTF-8' \
+    -H 'Accept: text/boolean' \
+    -d @rdf/data.trig
 sleep 300
 
 echo "namedgraphs texts.trig"
-curl -D- -H 'Content-Type: application/x-turtle; charset=UTF-8' \
-    -d @rdf/texts.trig \
-    -X POST $R_ENDPOINT_V
+curl $R_ENDPOINT_V \
+    -H 'Content-Type: application/x-trig; charset=UTF-8' \
+    -H 'Accept: text/boolean' \
+    -d @rdf/texts.trig
 sleep 300
 
 for i in {1..10}
     do
     echo "namedgraphs mentions${i}.trig"
-    curl -D- -H 'Content-Type: application/x-turtle; charset=UTF-8' \
-        -d @rdf/mentions$i.trig \
-        -X POST $R_ENDPOINT_V
+    curl $R_ENDPOINT_V \
+        -H 'Content-Type: application/x-trig; charset=UTF-8' \
+        -H 'Accept: text/boolean' \
+        -d @rdf/mentions$i.trig
     sleep 300
     done
